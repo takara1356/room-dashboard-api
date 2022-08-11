111847fd54d20834dbb303d6a0a1882ce2f3f3bd
takara1356 <goblin7283@gmail.com>
2022-08-11 21:26:40 +0900

    バグ修正

--- app/scripts/main.rb
+++ app/scripts/main.rb
@@ -7,7 +7,7 @@ class Main
 
     bme280 = JSON.parse(`python app/scripts/bme280.py`.gsub(/'/, "\""))
     # mh19 = JSON.parse(`python -m mh_z19`)
-    ConditionDegree.create!(temperature: mh19['temp'], humidity: mh19['humid'], pressure: mh19['pressuer'])
+    ConditionDegree.create!(temperature: bme280['temp'], humidity: bme280['humid'], pressure: bme280['pressuer'])
 
     puts 'end'
   end
